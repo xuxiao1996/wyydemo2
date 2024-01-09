57509b4be7c2df69b510e032858bad83c24e5356
xuxiao1996 <15754310423@163.com>
2024-01-09 22:57:49 +0800

    4-1底部播放器布局

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import {PlayCountPipe} from '../play-count.pipe';
+import {WyPlayerModule} from './wy-player/wy-player.module';
 
 
 
 @NgModule({
   declarations: [SingleSheetComponent, PlayCountPipe],
   imports: [
+    WyPlayerModule
   ],
-  exports: [SingleSheetComponent, PlayCountPipe],
+  exports: [
+    SingleSheetComponent,
+    PlayCountPipe,
+    WyPlayerModule,
+  ],
 })
 export class WyUiModule { }
