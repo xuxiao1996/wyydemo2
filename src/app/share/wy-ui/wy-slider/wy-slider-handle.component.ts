96527fb5de2db9d297271490dc0c0e96d47a3a4d
xuxiao1996 <15754310423@163.com>
2024-03-16 22:52:34 +0800

    4-5滑块组建（二）

--- src/app/share/wy-ui/wy-slider/wy-slider-handle.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-handle.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {WySliderStyle} from './wy-slider-types';
 
 @Component({
   selector: 'app-wy-slider-handle',
-  template: `<div class="wy-slider-handle"></div>`
+  template: `<div class="wy-slider-handle" [ngStyle]="style"></div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WySliderHandleComponent implements OnInit {
+export class WySliderHandleComponent implements OnInit, OnChanges {
+  @Input() wyVertical = false;
+  @Input() wyOffset: number;
+
+  style: WySliderStyle = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['wyOffset']) {
+      this.style[this.wyVertical ? 'bottom' : 'left'] = this.wyOffset + '%';
+    }
+    throw new Error('error');
   }
 
 }
--- src/app/share/wy-ui/wy-slider/wy-slider-track.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-track.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {WySliderStyle} from './wy-slider-types';
 
 @Component({
   selector: 'app-wy-slider-track',
-  template: `<div class="wy-slider-track"></div>`
+  template: `<div class="wy-slider-track" [ngStyle]="style"></div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WySliderTrackComponent implements OnInit {
+export class WySliderTrackComponent implements OnInit, OnChanges {
+  @Input() wyVertical = false;
+  @Input() wyLength: number;
+  style: WySliderStyle = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['wyLength']) {
+      this.style[this.wyVertical ? 'height' : 'width'] = this.wyLength + '%';
+      if (this.wyVertical) {
+        this.style.height = this.wyVertical + '%';
+        this.style.left = null;
+        this.style.width = null;
+      } else {
+        this.style.width = this.wyVertical + '%';
+        this.style.bottom = null;
+        this.style.height = null;
+      }
+    }
+    throw new Error('error');
+  }
 }
